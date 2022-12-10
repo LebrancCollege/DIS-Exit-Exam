a0113353093fc338d9533d59c3a372ef1c4186aa
lebrancconvas <lebrancconvas@gmail.com>
2022-12-10 22:36:50 +0700

    ♻️ refactor: change class component to functional component

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -1,23 +1,27 @@
+import type { NextPage } from 'next'; 
 import Head from 'next/head'; 
 import Link from 'next/link'; 
 import styles from '../styles/Home.module.css'; 
 
-export default function Home() {
+const Home: NextPage = () => {
   return(
     <div className={styles.home}> 
       <Head>
+        <link rel="icon" type="image/png" href="./img/icon/icon.png" /> 
         <title>UX Portfolio | Poom Yimyuean</title>
       </Head>
       <header>
-        <div className="text-3xl"> 
+        <div className="text-5xl">  
           <h1>UX Portfolio</h1> 
         </div>
       </header>
       <section>
-        <div>
+        <div className="text-2xl">  
           <h2>Poom Yimyuean</h2> 
         </div>
       </section>
     </div>
   )
 }
+
+export default Home; 
