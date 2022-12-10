1a1ba6c40c84b4503433e96cb1cb61e1e996cc85
lebrancconvas <lebrancconvas@gmail.com>
2022-12-10 21:00:11 +0700

    ✨ feat: add tailwind css

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -1,16 +1,23 @@
 import Head from 'next/head'; 
+import Link from 'next/link'; 
+import styles from '../styles/Home.module.css'; 
 
 export default function Home() {
   return(
-    <div>
+    <div className={styles.home}> 
       <Head>
         <title>UX Portfolio | Poom Yimyuean</title>
       </Head>
       <header>
-        <div>
+        <div className="text-3xl"> 
           <h1>UX Portfolio</h1> 
         </div>
       </header>
+      <section>
+        <div>
+          <h2>Poom Yimyuean</h2> 
+        </div>
+      </section>
     </div>
   )
 }
